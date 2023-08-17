9553b6a1b918f8ffca3a76fde4395b2ba3f45641
Vladislav <vladkitov2003@mail.ru>
2023-08-18 01:06:15 +0300

    full

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Blog_Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=2500)
     date = models.DateField(auto_now=False)
 
     objects = models.Manager()
